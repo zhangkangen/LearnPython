730473ef54d9bb4ba4de63ab3752a70232c09fe2
zhang.xiaokang <zhangkangen@gmail.com>
2015-12-05 19:01:00 +0800

    Initial commit

---
+++ FirstTest.py
@@ -0,0 +1,23 @@
+__author__ = 'xiaokang'
+print(100 + 200)
+print("hello world")
+print(ord('A'))
+classmeta = ['zhangsan', 'lisi', 'wangwu']
+print(classmeta)
+print(len(classmeta))
+print(classmeta[0])
+age = 20
+if age > 18:
+    print('ok')
+sum = 0
+for x in range(101):
+    sum+=x
+print(sum)
+
+def my_abs(x):
+    if x>=0:
+        return x
+    else:
+        return -x
+print(my_abs(-2))
+
